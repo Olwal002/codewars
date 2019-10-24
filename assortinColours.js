a1ee48a3d3e32a666a475aa641f755a633b0ac82
olwal002 <olwalevance0@gmail.com>
2019-10-24 10:42:49 +0300

    assorting the colours in rainbow using the .join()method

---
+++ assortinColours.js
@@ -0,0 +1,7 @@
+// Is it a rainbow?
+
+function sortRainbow(colour){
+    
+    var data=['red','orange','yellow','green','blue','indigo','violet'];
+    return data.join("") === colour.join("");
+    }
